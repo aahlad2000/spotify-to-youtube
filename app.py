5b347f36300fcd8e2eafd46bc6de8b3b0e34582d
aahlad2000 <aahlad2000>
2024-12-25 22:48:01 +0530

    Boilerplate Firebase setup

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db.database import engine
 from db.models import Base
+# import firebase_admin
+# from firebase_admin import credentials, firestore
+
+# cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# firebase_admin.initialize_app(cred)
+
 # Initialize the FastAPI app
 app = FastAPI(
     title="SpotifyToYoutube",
     description="Spotify to Youtube API",
     version="0.1.0",
 )
+
+# cred = credentials.Certificate("firebase-credentials.json")
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
 @app.on_event("startup")
 async def startup():
     async with engine.begin() as conn:
